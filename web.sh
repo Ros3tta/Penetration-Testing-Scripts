6404353e3c23e24fe17bc62e4872989f31eef197
Rosetta <130989909+Ros3tta@users.noreply.github.com>
2025-07-03 13:22:47 +0300

    Create web.sh

---
+++ web.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <target_ip> <port>"
+    exit 1
+fi
+
+target="$1"
+port="$2"
+
+# Use HTTPS by default on common HTTPS ports
+if [[ "$port" == "443" || "$port" == "8443" || "$port" == "9443" ]]; then
+    proto="https"
+else
+    proto="http"
+fi
+
+url="$proto://$target:$port"
+
+echo "Scanning $url"
+
+wafw00f -a "$url"
+echo -e ""
+curl -skIL "$url"
+curl -skIL -X OPTIONS "$url"
+whatweb -a 3 "$url"
